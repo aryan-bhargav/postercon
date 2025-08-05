e3f2da91b0daa5374f58f9e0713a0d0feeeb4544
aryan-bhargav <bhargavaryan5@gmail.com>
2025-08-05 23:12:24 +0530

    term

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Terms = () => {
   return (
-    <div className="terms min-h-screen mx-45 px-4 py-10 text-left text-page">
+    <div className="refund-policy min-h-screen mx-5 md:mx-30 lg:mx-45 px-4 py-10 text-left text-page">
       <br />
       <br />
       <br />
